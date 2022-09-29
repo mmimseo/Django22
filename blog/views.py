7ba6050dff8bb3f27129e286602370734c005bf3
Baek Minseo <qoralstj2433@gmail.com>
2022-09-29 13:09:30 +0900

    CBV로 포스트 목록/ 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    #템플릿은 모델명_list.html : post_list.html
 
+class PostDetail(DetailView):
+    model = Post
+    # 템플릿은 모델명_detail.html : post_detail.html
+
+
+'''
 def index(request):
     posts = Post.objects.all().order_by('-pk')
 
@@ -12,6 +23,8 @@ def index(request):
 def single_post_page(request,pk):
     post1 = Post.objects.get(pk=pk)
     return render(request, 'blog/single_post_page.html',
-                  {'post': post1})
+                  {'post': post1}) 
+'''
+
 
 
--- single_pages/views.py
+++ single_pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def langing(request):
+def landing(request):
     return render(request, 'single_pages/landing.html')
 
 def about_me(request):
