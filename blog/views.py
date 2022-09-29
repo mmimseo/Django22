c0342afbe8bbfd7680b28e0e970d63366c342757
Baek Minseo <qoralstj2433@gmail.com>
2022-09-29 12:44:11 +0900

    FBV로 포스트 목록/상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ def index(request):
                   {'posts': posts})
 
 def single_post_page(request,pk):
-    post = Post.objects.get(pk=pk)
+    post1 = Post.objects.get(pk=pk)
     return render(request, 'blog/single_post_page.html',
                   {'post': post1})
 
