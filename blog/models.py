c551976dd42d13bc3c70890798848bcd1a072f72
mmimseo <qoralstj2433@gmail.com>
2022-10-24 21:41:05 +0900

    포스트 목록/상세 페이지에 author 출력하기

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 import os
 
 # Create your models here.
+
 class Post(models.Model):
     title = models.CharField(max_length=30)  #포스트 제목
     hook_text = models.CharField(max_length=100, blank=True)
