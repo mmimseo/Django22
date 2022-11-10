a9cbef0745393a3e567150ae9500477e6fa2dabd
Baek Minseo <qoralstj2433@gmail.com>
2022-11-10 12:16:18 +0900

    포스트 수정페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render, redirect
-from .models import Post, Category, Tag
-from django.views.generic import ListView, DetailView, CreateView  ## CBV ##
+from django.views.generic import ListView, DetailView, CreateView, UpdateView  ## CBV ##
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post, Category, Tag
+from django.core.exceptions import PermissionDenied
 # Create your views here.
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    template_name = 'blog/post_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+    def get_context_data(self, *, object_list=None, **kwargs):  ##추가인자##
+        context = super(PostUpdate,self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count
+        return context
 
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
