b29cc34965802e8564633bcf71ea7f34f88b06b9
Baek Minseo <qoralstj2433@gmail.com>
2022-11-15 15:07:40 +0900

    태그 선택란 추가하기

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,12 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView  #
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Category, Tag
 from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
 # Create your views here.
 
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
     template_name = 'blog/post_update_form.html'
 
     def dispatch(self, request, *args, **kwargs):
@@ -16,8 +17,29 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
         else:
             raise PermissionDenied
 
+    def form_valid(self, form):
+        response = super(PostUpdate, self).form_valid(form)
+        tags_str = self.request.POST.get('tags_str')
+        if tags_str:
+            tags_str = tags_str.strip()
+            tags_str = tags_str.replace(',', ';')
+            tags_list = tags_str.split(';')
+            for t in tags_list :
+                t = t.strip()
+                tag, is_tag_created = Tag.objects.get_or_create(name = t)
+                if is_tag_created:
+                    tag.slug = slugify(t, allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
+        return response
+
     def get_context_data(self, *, object_list=None, **kwargs):  ##추가인자##
         context = super(PostUpdate,self).get_context_data()
+        if self.object.tags.exixt:
+            tag_str_list = list()
+            for t in self.object.tags.all():
+                tag_str_list.append(t.name)
+            context['tags_str_default'] = ';'.join(tag_str_list)
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count
         return context
@@ -34,7 +56,20 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         current_user = self.request.user
         if current_user.is_authenticated and (current_user.is_superuser or current_user.is_staff):      # and 둘 다 만족
             form.instance.author = current_user
-            return super(PostCreate, self).form_valid(form)
+            response = super(PostCreate, self).form_valid(form)
+            tags_str = self.request.POST.get('tags_str')
+            if tags_str:
+                tags_str = tags_str.strip()
+                tags_str = tags_str.replace(',', ';')
+                tags_list = tags_str.split(';')
+                for t in tags_list :
+                    t = t.strip()
+                    tag, is_tag_created = Tag.objects.get_or_create(name = t)
+                    if is_tag_created:
+                        tag.slug = slugify(t, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)
+            return response
         else:
             return redirect('/blog/')
     def get_context_data(self, *, object_list=None, **kwargs):  ##추가인자##
